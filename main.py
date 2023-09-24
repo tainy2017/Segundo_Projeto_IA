1c187963cd0d0c5185ba65b6092cb7833ca0f200
Magda Amaral <magdaamaral2014@yahoo.com.br>
2023-09-24 15:56:55 -0300

    Projeto de IA

---
+++ main.py
@@ -0,0 +1,20 @@
+#pip install numpy
+
+import matplotlib.pyplot as plt
+import numpy as np
+import cv2
+#from tensorflow.keras.models import load_model
+from tensorflow import keras
+from keras.layers import Dense
+from keras.models import Sequential, load_model
+from imutils.contours import sort_contours
+import imutils
+
+rede_neural = load_model('rede_neural/rede_neural')
+rede_neural.summary()
+
+img = cv2.imread('imagens/teste-manuscrito01.jpg')
+plt.imshow(img)
+gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+plt.imshow(gray)
+
